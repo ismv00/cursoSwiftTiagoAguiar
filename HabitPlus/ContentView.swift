3b9ef34e73134c31b9d747462da8352bb9e078b5
ismv00 <ismv00@icloud.com>
2022-08-27 09:30:43 -0400

    Initial Commit

---
+++ HabitPlus/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HabitPlus
+//
+//  Created by Igor S. Menezes on 27/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HabitPlus/HabitPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitPlusApp.swift
+//  HabitPlus
+//
+//  Created by Igor S. Menezes on 27/08/22.
+//
+
+import SwiftUI
+
+@main
+struct HabitPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
